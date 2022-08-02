40fbf363c89e1ef9308cfdd62b88afc3608b439e
Jake Ashton <jakemaddenashton@gmail.com>
2022-08-02 11:26:11 -0700

    Make button reset all squares to white

--- javascript.js
+++ javascript.js
@@ -8,7 +8,6 @@ function createGrid() {
   }
 
   const squares = document.querySelectorAll('div#container > div');
-
   squares.forEach(square => square.addEventListener('mouseenter', (e) => {
   square.classList.add('color');
 }));
@@ -17,9 +16,11 @@ function createGrid() {
 createGrid();
 
 function resize() {
+  const squares = document.querySelectorAll('div#container > div');
   gridValue = parseInt(prompt('Select grid dimension up to 100'));
   container.style.height = '650px';
   value = `repeat(${gridValue}, ${640 / gridValue})`;
   container.setAttribute('style', `grid-template-rows: repeat(${gridValue}, ${640 / gridValue}px); grid-template-columns: repeat(${gridValue}, ${640 / gridValue}px);`);
+  squares.forEach(square => square.classList.remove('color'));
   createGrid();
 }
