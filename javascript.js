7b8ef1921aad39a52dec757bbec11fbcfedfbef3
Jake Ashton <jakemaddenashton@gmail.com>
2022-08-01 20:53:13 -0700

    Add color change on hover

--- javascript.js
+++ javascript.js
@@ -5,3 +5,9 @@ for (let i = 0; i < gridValue ** 2; i++) {
     let div = document.createElement('div');
     container.append(div);
 }
+
+const squares = document.querySelectorAll('div#container > div');
+
+squares.forEach(square => square.addEventListener('mouseenter', (e) => {
+  square.classList.add('color');
+}));
