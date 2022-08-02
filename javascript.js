f59bed3145ecda15a530a95d5dfaaffb60ad47af
Jake Ashton <jakemaddenashton@gmail.com>
2022-08-02 12:14:29 -0700

    Add comments

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
+// Establish DOM container object and set initial grid value
 const container = document.getElementById("container");
 let gridValue = 16;
 
 function createGrid() {
+  // Add squares to container
   for (let i = 0; i < gridValue ** 2; i++) {
       let div = document.createElement('div');
       container.append(div);
   }
 
+  // Add color to squares when hovered over
   const squares = document.querySelectorAll('div#container > div');
   squares.forEach(square => square.addEventListener('mouseenter', (e) => {
   square.classList.add('color');
@@ -16,11 +19,14 @@ function createGrid() {
 createGrid();
 
 function resize() {
-  const squares = document.querySelectorAll('div#container > div');
+  // Prompt for new grid value 1 - 100
   gridValue = parseInt(prompt('Select grid dimension up to 100'));
   while (gridValue < 1 || gridValue > 100) {
     gridValue = parseInt(prompt('Select grid dimension up to 100'));
   }
+
+  // Modify size of squares and reset grid to white
+  const squares = document.querySelectorAll('div#container > div');
   container.setAttribute('style', `grid-template-rows: repeat(${gridValue}, ${640 / gridValue}px); grid-template-columns: repeat(${gridValue}, ${640 / gridValue}px);`);
   squares.forEach(square => square.classList.remove('color'));
   createGrid();
