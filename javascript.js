b6f9fa2ea177d561859b965e0ecb35680bfc6139
Jake Ashton <jakemaddenashton@gmail.com>
2022-08-02 11:32:23 -0700

    Reprompt user if resolution out of allowable range

--- javascript.js
+++ javascript.js
@@ -18,8 +18,9 @@ createGrid();
 function resize() {
   const squares = document.querySelectorAll('div#container > div');
   gridValue = parseInt(prompt('Select grid dimension up to 100'));
-  container.style.height = '650px';
-  value = `repeat(${gridValue}, ${640 / gridValue})`;
+  while (gridValue < 1 || gridValue > 100) {
+    gridValue = parseInt(prompt('Select grid dimension up to 100'));
+  }
   container.setAttribute('style', `grid-template-rows: repeat(${gridValue}, ${640 / gridValue}px); grid-template-columns: repeat(${gridValue}, ${640 / gridValue}px);`);
   squares.forEach(square => square.classList.remove('color'));
   createGrid();
