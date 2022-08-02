131ad36be6e30c21e76a5a7549bb84c42b468ee6
Jake Ashton <jakemaddenashton@gmail.com>
2022-08-02 11:03:11 -0700

    Add resize functionality to button click

--- javascript.js
+++ javascript.js
@@ -1,13 +1,25 @@
 const container = document.getElementById("container");
 let gridValue = 16;
 
-for (let i = 0; i < gridValue ** 2; i++) {
-    let div = document.createElement('div');
-    container.append(div);
-}
+function createGrid() {
+  for (let i = 0; i < gridValue ** 2; i++) {
+      let div = document.createElement('div');
+      container.append(div);
+  }
 
-const squares = document.querySelectorAll('div#container > div');
+  const squares = document.querySelectorAll('div#container > div');
 
-squares.forEach(square => square.addEventListener('mouseenter', (e) => {
+  squares.forEach(square => square.addEventListener('mouseenter', (e) => {
   square.classList.add('color');
 }));
+}
+
+createGrid();
+
+function resize() {
+  gridValue = parseInt(prompt('Select grid dimension up to 100'));
+  container.style.height = '650px';
+  value = `repeat(${gridValue}, ${640 / gridValue})`;
+  container.setAttribute('style', `grid-template-rows: repeat(${gridValue}, ${640 / gridValue}px); grid-template-columns: repeat(${gridValue}, ${640 / gridValue}px);`);
+  createGrid();
+}
